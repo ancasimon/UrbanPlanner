ecaf8476355e2dc6eec1f2f99bc447f32d991f18
Anca Simon <ancarsimon@gmail.com>
2020-08-08 11:01:02 -0500

    Add project files.

---
+++ UrbanPlanner/Building.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UrbanPlanner  //ANCA: Note: Initially, I had Building as a folder name too > which made it a namespace - it did't like that there was a namespace and a class with the same name and could not use the class/type!!
+{
+    class Building
+    {
+        //private fields:
+        public string _designer = "Anca";
+        public DateTime _dateConstructed;  //ANCA: This gets set by the Construct() method below!
+        public string _address;
+        public string _owner;
+
+        //public properties:
+        public int Stories { get; set; }
+        public double Width { get; set; }
+        public double Depth { get; set; }
+        public double Volume {                                    //Anca's note: Volume has to be of the type double because it uses double in its calculation!
+            get { return Width * Depth * (3 * Stories); }
+                }
+
+        //constructor:
+        public Building(string address)
+        {
+            _address = address;
+            Console.WriteLine($"Instantiated a new building: {_address}.");
+        }
+
+
+        //public methods:
+        public void Construct()
+        {
+            _dateConstructed = DateTime.Now;
+            Console.WriteLine($"{_address} was built on {_dateConstructed}.");
+        }
+
+        public void Purchase(string owner)
+        {
+            _owner = owner;
+            Console.WriteLine($@"
+{_address}
+Designed by {_designer}
+Constructed on {_dateConstructed}
+Owned by {_owner}
+{Volume} cubic meters of space
+");
+        }
+
+
+    }
+}
---
+++ UrbanPlanner/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace UrbanPlanner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //create buildings:
+            var Building123 = new Building("123 Linden Ave") //ANCA: why start with the class name rather than var?????(in the example int he exercise!)
+            {
+                Width = 200,
+                Depth = 200,
+                Stories = 2
+            };
+
+            var Building456 = new Building("456 12th Ave S")
+            {
+                Width = 100,
+                Depth = 100,
+                Stories = 1
+            };
+
+            var Building789 = new Building("789 Broadway")
+            {
+                Width = 150,
+                Depth = 150,
+                Stories = 4
+            };
+
+
+            //construct the buildings:
+            Building123.Construct();
+            Building456.Construct();
+            Building789.Construct();
+
+            Building123.Purchase("Lamar Alexander");
+            Building456.Purchase("Jim Cooper");
+            Building789.Purchase("Nashville Symphony");
+
+
+
+
+        }
+    }
+}
